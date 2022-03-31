0ec4262cc5d00856cb27056d95cac9e3acc466d9
Méchain Cyril <macmc@tutanota.com>
2022-03-31 20:08:04 +0200

    Up to 1.0.8

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,26 +4,24 @@ import "../../styles.css";
 export function SimplyModal({ content, isOpen }) {
   const [open, setOpen] = useState(isOpen);
 
-  let containerModal;
-
-  if (open === false) {
-    containerModal = "containerModal close";
-  } else {
-    containerModal = "containerModal";
-  }
-
   function closingModal() {
     setOpen(false);
   }
 
-  return (
-    <div className={containerModal}>
-      <div className="modal">
-        <div className="modal__closeButton" onClick={() => closingModal()} />
-        <h2>{content}</h2>
+  if (open === true) {
+    modal = (
+      <div className="containerModal">
+        <div className="modal">
+          <div className="modal__closeButton" onClick={closingModal}></div>
+          <h2>{content}</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    modal = null;
+  }
+
+  return modal;
 }
 
 export default SimplyModal;
