b85c2516691f4fa86265c08aefb4c392349a510a
Méchain Cyril <macmc@tutanota.com>
2022-04-03 14:56:46 +0200

    Up to 1.1.9

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from "react";
 import "../../styles.css";
 
 export function SimplyModal({ content, isOpen }) {
-  const [open, setOpen] = useState();
-  const [containerModal, setContainerModal] = useState();
+  const [open, setOpen] = useState(isOpen);
+  let modal;
 
-  useEffect(() => {
+  /*useEffect(() => {
     setOpen(isOpen);
-  });
-
-  if (open === true) {
-    setContainerModal("containerModal");
-  } else {
-    setContainerModal("containerModal close");
-  }
+  });*/
 
   function closingModal() {
     setOpen(false);
   }
 
-  return (
-    <div className={containerModal}>
-      <div className="modal">
-        <div className="modal__closeButton" onClick={closingModal}></div>
-        <h2>{content}</h2>
+  if (open === true) {
+    modal = (
+      <div className="containerModal">
+        <div className="modal">
+          <div className="modal__closeButton" onClick={closingModal}></div>
+          <h2>{content}</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    modal = null;
+  }
+
+  return modal;
 }
 
 export default SimplyModal;
