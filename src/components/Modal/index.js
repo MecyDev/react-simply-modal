69c3a14c2fdb09c21e28badc5ed9cda54e39b416
Méchain Cyril <macmc@tutanota.com>
2022-04-04 17:17:15 +0200

    Add Echap for close and css adjust

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles.css";
 
 function SimplyModal({ isOpen, visible, content }) {
   let modal;
+
+  useEffect(() => {
+    const close = (e) => {
+      if (e.keyCode === 27) {
+        visible();
+      }
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
 
   if (isOpen === true) {
     modal = (
@@ -13,7 +23,12 @@ function SimplyModal({ isOpen, visible, content }) {
           aria-modal="true"
           aria-labelledby="heading"
         >
-          <div className="modal__closeButton" onClick={visible}></div>
+          <button
+            type="button"
+            className="modal__closeButton"
+            onClick={visible}
+            aria-label="Close"
+          ></button>
           <h2 id="heading">{content}</h2>
         </div>
       </div>
